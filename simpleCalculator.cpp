0c59232375d2fd4f8c49548808aa19b819ab99ec
Laert Llaveshi <l.llaveshi@tum.de>
2026-03-10 14:43:24 +0100

    added multiply and divide to simpleCalculator.cpp

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
+double multiply(double a, double b)
+{
+  return a * b;
+}
+
+double divide(double a, double b)
+{
+  if (b != 0)
+    return a / b;
+  else
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-  std::cout << "Hello World!\n";
-    return 0;
+  double a = 4;
+  double b = 2;
+  double mul = multiply(a, b);
+  double div = divide(a, b);
+  std::cout << "The value is " << mul << "\n";
+  std::cout << "The value is " << div << "\n";
+  return 0;
 }
