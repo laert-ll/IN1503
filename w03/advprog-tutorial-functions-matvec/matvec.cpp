3408e76adb0192e7d07b264a13072341b6b72010
Laert Llaveshi <l.llaveshi@tum.de>
2026-03-28 15:50:28 +0100

    w03

--- w03/advprog-tutorial-functions-matvec/matvec.cpp
+++ w03/advprog-tutorial-functions-matvec/matvec.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 
-void print_vector(const std::vector<int>& v) {
+template <typename T>
+void print_vector(const std::vector<T>& v) {
   std::cout << " [ ";
   for (const auto& elem : v) {
     std::cout << elem << " ";
@@ -9,15 +10,8 @@ void print_vector(const std::vector<int>& v) {
   std::cout << "]" << std::endl;
 }
 
-void print_vector(const std::vector<double>& v) {
-  std::cout << " [ ";
-  for (const auto& elem : v) {
-    std::cout << elem << " ";
-  }
-  std::cout << "]" << std::endl;
-}
-
-void print_matrix(const std::vector<std::vector<int>>& A) {
+template <typename T>
+void print_matrix(const std::vector<std::vector<T>>& A) {
   std::cout << "[" << std::endl;
   for (const auto& row : A) {
     for (const auto& elem : row) {
@@ -28,60 +22,22 @@ void print_matrix(const std::vector<std::vector<int>>& A) {
   std::cout << "]" << std::endl;
 }
 
-void print_matrix(const std::vector<std::vector<double>>& A) {
-  std::cout << "[" << std::endl;
-  for (const auto& row : A) {
-    for (const auto& elem : row) {
-      std::cout << elem << " ";
+std::vector<double> operator* (const std::vector<std::vector<double>>& A, const std::vector<double>& v) {
+  const auto N = v.size();
+  std::vector<double> res(N, 0.0);
+  for (auto i = 0u; i < N; i++) {
+    for (auto j = 0u; j < N; j++) {
+      res[i] += A[i][j] * v[j];
     }
-    std::cout << std::endl;
   }
-  std::cout << "]" << std::endl;
+  return res;
 }
 
 int main() {
+  std::vector<std::vector<double>> matrix = {{1., 0., 0.}, {0., 1., 0.}, {0., 0., 1.}};
 
-  // Get the vector elements from the user
-  std::vector<int> v{};
-  std::cout << "Give elements to the vector v\n";
-  int x;
-  while (std::cin >> x) {
-    v.push_back(x);
-  }
-  std::cin.clear();
-  std::cin.ignore();
+  std::vector<double> vec_a = {1., 2., 3.};
 
-  // Print the vector v
-  std::cout << "Vector v is:\n";
-  print_vector(v);
-
-  // Get a square matrix A
-  const auto N = v.size();
-  std::cout << "Give the " << N * N << " elements of matrix A (row-first)\n";
-  std::vector<std::vector<int>> A(N); // We define just the lines for now
-  for (auto i = 0u; i < N; ++i) {     //  0u means 0 of unsigned integer type - to avoid a conversion warning
-    for (auto j = 0u; j < N; ++j) {
-      int y;
-      std::cin >> y;
-      A[i].push_back(y);
-    }
-  }
-
-  // Print the matrix A
-  std::cout << "Matrix A is:" << std::endl;
-  print_matrix(A);
-
-  // Multiply A * v and store it to a vector m
-  std::vector<int> m(N, 0);
-  for (auto i = 0u; i < N; ++i) {
-    for (auto j = 0u; j < N; ++j) {
-      m[i] += A[i][j] * v[j];
-    }
-  }
-
-  // Print the vector m
-  std::cout << "Vector m = A * v is:" << std::endl;
-  print_vector(m);
-
-  return 0;
+  auto vec_mul = matrix * vec_a;
+  print_vector(vec_mul);
 }
